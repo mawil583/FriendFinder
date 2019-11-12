1e810d9f5b7465e51b1a2d1b17ccc6b8c8186433
Michael Williams <mwilliams2964@gmail.com>
2019-11-12 15:23:30 -0700

    added 10 players to friends array

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,1 +1,52 @@
-questionsArr = [];
+let friends = [
+    {
+        name: "John Wayne",
+        photo: "John.png",
+        scores: [1,4,5,2,3,4,1,3,2,5]
+    },
+    {
+        name: "Tim Burton",
+        photo: "Tim.png",
+        scores: [1,4,2,2,5,1,1,2,1,5]
+    },
+    {
+        name: "Brittany Joice",
+        photo: "Brittany.png",
+        scores: [1,1,2,1,4,1,3,5,4,3]
+    },
+    {
+        name: "Thomas Paine",
+        photo: "Thomas.png",
+        scores: [2,4,3,4,1,2,1,5,4,4]
+    },
+    {
+        name: "Joshua Williams",
+        photo: "Joshua.png",
+        scores: [1,2,3,2,4,2,5,5,3,5]
+    },
+    {
+        name: "Ingram Wellington",
+        photo: "Ingram.png",
+        scores: [3,4,2,5,3,5,5,1,4,2]
+    },
+    {
+        name: "Janice Holmes",
+        photo: "Janice.png",
+        scores: [5,1,4,2,3,4,2,4,5,3]
+    },
+    {
+        name: "Rafael Katz",
+        photo: "Rafael.png",
+        scores: [5,3,4,5,4,5,5,5,5,4]
+    },
+    {
+        name: "Albert Einstein",
+        photo: "Albert.png",
+        scores: [4,3,5,4,5,2,3,3,1,4]
+    },
+    {
+        name: "Dejana Tintor",
+        photo: "Dejana.png",
+        scores: [4,3,5,4,5,2,3,3,1,4]
+    }
+];
