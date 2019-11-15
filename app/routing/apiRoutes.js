0c8a6b10de0e8559e4187c534a9af2107a1b56dc
Michael Williams <mwilliams2964@gmail.com>
2019-11-14 18:07:47 -0700

    deploy heroku

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,36 +1,61 @@
 
 let friends = require("../data/friends");
 
-module.exports = function(app) {
-    app.get("/api/friends", function(req, res) {
+module.exports = function (app) {
+    app.get("/api/friends", function (req, res) {
         // res is the response from the server 
         // to the client which will be displayed
         // in the form of a new json object {friends}
-        res.json(friends);  
+        res.json(friends);
     });
-    app.post("/api/friends", function(req, res) {
+    app.post("/api/friends", function (req, res) {
         /* post creates */
         // console.log(req.body);
         // res.json({name: "michael"});
         let newFriend = req.body;
         console.log("about to add a new friend to array")
+        console.log(`newFriend:`, newFriend);
         console.log(friends.length);
         // game logic goes here
         // first find the friends array and compare their answer
         // array to yours. Take absolute value of difference and
         // then compare them
-        
+
         // answer array
 
+        let matchDiff = [];
         for (let i = 0; i < friends.length; i++) {
-            let matchDiff = 0;
-            for (let j = 0; j < friends[i].scores.length; i++) {
-                if (newFriend.scores[j] !== friends[i].scores[j]) {
-                    
-                }
+            for (let j = 0; j < 10; j++) {
+                // if current answer index of new friend does not 
+                // equal current answer index of a friend at some
+                // index within friends array of objcets, then
+                // take the absolute value of the difference of 
+                // newFriend score and friend[i] score
+
+                // if (parseInt(newFriend.scores[j]) !== parseInt(friends[i].scores[j])) 
+                console.log('is there an error yet?')
+                console.log(`newFriend.scores[j]: ${parseInt(newFriend.answers[j])}`);
+                console.log(`friends[i].scores[j]: ${parseInt(friends[i].scores[j])}`);
+                let personDiff = Math.abs(parseInt(newFriend.answers[j]) - parseInt(friends[i].scores[j]));
+                console.log('how about now?')
+                matchDiff.push(personDiff);
+                console.log('now?')
             }
         }
+        console.log('and now?')
 
+        console.log(matchDiff);
+        console.log("orrrr")
+        let index = 0;
+        let smallestVal = matchDiff[0];
+        for (let k = 0; k < matchDiff.length; k++) {
+            if (matchDiff[k] < smallestVal) {
+                smallestVal = matchDiff[k];
+                index = k;
+            };
+        }
+        console.log(`matchDiff array: ${matchDiff}`);
+        console.log(`closest match is person at index ${index}`);
         friends.push(newFriend);
         console.log("new friend added");
         console.log(friends.length);
