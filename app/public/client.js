9fd3bf208133f9691233fbe5edbce4efac951052
Michael Williams <mwilliams2964@gmail.com>
2019-11-16 17:32:14 -0700

    cleaned up

--- app/public/client.js
+++ app/public/client.js
@@ -21,7 +21,7 @@ $(document).ready(function () {
                 $('#myModal').modal('show');
             });
         $("#name").val("");
-        $("#photo").val('');
+        $("#photo").val("");
         $('select').val("");
     });
 });
--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -8,12 +8,3 @@ module.exports = function(app) {
         res.sendFile(path.join(__dirname, "../public/survey.html"));
     });
 };
-// request is going to be the information
-/* received from the client whereas,
-response is an object that. 
-
-You can send a 
-request from the client (browser and/or
-front end code) and the server can send a 
-response back to the client. request is data
-(us being the server) */
